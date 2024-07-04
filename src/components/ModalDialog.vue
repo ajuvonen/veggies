c15fd89c9c7a8b28adcf0dabe528351aa9ceadb3
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-07-04 13:43:55 +0300

    Add an info dialog to front page

---
+++ src/components/ModalDialog.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import {Dialog, DialogPanel, DialogTitle, TransitionRoot} from '@headlessui/vue';
+
+defineEmits(['close']);
+defineProps<{
+  open: boolean;
+  title: string;
+}>();
+</script>
+<template>
+  <TransitionRoot
+    :show="open"
+    as="template"
+    enter="duration-200 ease-out"
+    enter-from="opacity-0"
+    enter-to="opacity-100"
+    leave="duration-200 ease-in"
+    leave-from="opacity-100"
+    leave-to="opacity-0"
+  >
+    <Dialog @close="$emit('close')" class="relative z-10">
+      <!-- The backdrop, rendered as a fixed sibling to the panel container -->
+      <div class="modal-dialog__backdrop" aria-hidden="true" />
+      <!-- Full-screen container to center the panel -->
+      <div class="modal-dialog__container">
+        <DialogPanel class="modal-dialog">
+          <DialogTitle as="h2" class="modal-dialog__title">{{ title }}</DialogTitle>
+          <div class="mt-4">
+            <slot name="content"></slot>
+          </div>
+          <div v-if="$slots.buttons" class="modal-dialog__buttons">
+            <slot name="buttons"></slot>
+          </div>
+        </DialogPanel>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+<style lang="scss" scoped>
+.modal-dialog__backdrop {
+  @apply fixed inset-0;
+  @apply bg-black/30;
+}
+
+.modal-dialog__container {
+  @apply fixed inset-0 w-screen p-4;
+  @apply flex items-center justify-center;
+}
+
+.modal-dialog {
+  @apply w-full max-w-md transform overflow-hidden rounded-md p-4 align-middle shadow-xl;
+  @apply bg-slate-100;
+}
+
+.modal-dialog__title {
+  @apply text-lg font-medium leading-6;
+  @apply text-gray-900;
+}
+
+.modal-dialog__buttons {
+  @apply mt-4;
+  @apply flex gap-2 justify-end;
+}
+</style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import {ref} from 'vue';
 import {storeToRefs} from 'pinia';
 import {DateTime} from 'luxon';
 import {useActivityStore} from '@/stores/activityStore';
 import Button from '@/components/Button.vue';
+import ModalDialog from '@/components/ModalDialog.vue';
 
 const {settings} = storeToRefs(useActivityStore());
+
+const dialogOpen = ref(false);
 
 const start = () => {
   settings.value.startDate = DateTime.now().startOf('week');
@@ -15,9 +19,18 @@ const start = () => {
   <p class="welcome__paragraph">
     {{ $t('welcome.paragraph') }}
   </p>
-  <p class="welcome__paragraph">
+  <p class="welcome__paragraph flex gap-2">
+    <Button @click="dialogOpen = true">{{ $t('welcome.info') }}</Button>
     <Button @click="start()">{{ $t('general.start') }}</Button>
   </p>
+  <ModalDialog :open="dialogOpen" :title="$t('welcome.infoTitle')" @close="dialogOpen = false">
+    <template #content>
+      {{ $t('welcome.infoContent') }}
+    </template>
+    <template #buttons>
+      <Button @click="dialogOpen = false">{{ $t('general.close') }}</Button>
+    </template>
+  </ModalDialog>
 </template>
 <style scoped lang="scss">
 .welcome__title {
