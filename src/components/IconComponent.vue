9ff3df50c761144561ef9525fc6b7621be48a01c
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-07-04 16:31:41 +0300

    Add icon component

---
+++ src/components/IconComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {mdiCogOutline, mdiTrashCan} from '@mdi/js';
+
+type IconString = 'cog' | 'trashCan';
+
+defineProps<{
+  icon: IconString;
+}>();
+
+const icons = {
+  cog: mdiCogOutline,
+  trashCan: mdiTrashCan,
+};
+</script>
+<template>
+  <svg viewBox="0 0 24 24" aria-hidden="true" class="w-5 h-5">
+    <path :d="icons[icon]" class="transition-all"></path>
+  </svg>
+</template>
