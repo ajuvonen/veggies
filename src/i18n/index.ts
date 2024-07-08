2f619c714cc7b5f999e5ab139fafcac0dbbb951c
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-07-08 10:57:45 +0300

    Change i18n typing

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,11 @@
 import {createI18n} from 'vue-i18n';
 import en from './en.json';
 import fi from './fi.json';
+import type {Locale} from '@/utils/types';
 
 type MessageSchema = typeof en;
 
-export default createI18n<[MessageSchema], 'en' | 'fi'>({
+export default createI18n<[MessageSchema], Locale>({
   locale: 'en',
   fallbackLocale: 'en',
   legacy: false,
