363cd246f2157c5f8c2b58cb89c7a656a5b9ebab
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-07-09 11:40:58 +0300

    Add week status

---
+++ src/components/WeekStatus.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import useDateTime from '@/hooks/dateTime';
+
+const {getTotalWeeks, getDateInterval} = useDateTime();
+</script>
+<template>
+  <div class="week-status">
+    <h1 class="text-xl uppercase">
+      {{ $t('general.weekInterval', [getTotalWeeks]) }}
+    </h1>
+    <span class="text-xs">{{ getDateInterval(getTotalWeeks - 1) }}</span>
+  </div>
+</template>
+<style lang="scss" scoped>
+.week-status {
+  @apply flex flex-col gap-2 items-center justify-center;
+  @apply pb-4;
+}
+</style>
--- src/views/LogView.vue
+++ src/views/LogView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import VeggieSearch from '@/components/VeggieSearch.vue';
+import WeekStatus from '@/components/WeekStatus.vue';
 </script>
 <template>
+  <WeekStatus />
   <VeggieSearch />
 </template>
