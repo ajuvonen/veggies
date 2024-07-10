f211e56985b4c89104404dbf5d1d7166e96228d8
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-07-10 13:59:33 +0300

    Test store methods

--- src/components/__tests__/activityStore.spec.ts
+++ src/components/__tests__/activityStore.spec.ts
@@ -37,4 +37,47 @@ describe('activityStore', () => {
     activityStore.toggleIngredient({key: 'tomato', category: Category.Vegetable});
     expect(activityStore.activities[0]).toEqual(lastWeekAction);
   });
+
+  it("shows this week's ingredients", () => {
+    activityStore.settings.startDate = DateTime.now().startOf('week').minus({weeks: 1});
+    activityStore.activities.push({
+      ingredient: {key: 'apple', category: Category.Fruit},
+      date: DateTime.now().startOf('week').minus({days: 1}),
+    });
+    activityStore.toggleIngredient({key: 'cucumber', category: Category.Vegetable});
+    activityStore.toggleIngredient({key: 'tomato', category: Category.Vegetable});
+    expect(activityStore.getCurrentIngredients.length).toBe(2);
+    expect(
+      activityStore.getCurrentIngredients.some((ingredient) => ingredient.key === 'tomato'),
+    ).toBe(true);
+
+    expect(
+      activityStore.getCurrentIngredients.some((ingredient) => ingredient.key === 'cucumber'),
+    ).toBe(true);
+  });
+
+  it("shows specific week's ingredients", () => {
+    activityStore.settings.startDate = DateTime.now().startOf('week').minus({weeks: 1});
+    activityStore.activities.push({
+      ingredient: {key: 'apple', category: Category.Fruit},
+      date: DateTime.now().startOf('week').minus({days: 1}),
+    });
+    activityStore.toggleIngredient({key: 'cucumber', category: Category.Vegetable});
+    activityStore.toggleIngredient({key: 'tomato', category: Category.Vegetable});
+
+    expect(activityStore.getIngredientsForWeek(0).length).toBe(1);
+    expect(
+      activityStore.getIngredientsForWeek(0).some((ingredient) => ingredient.key === 'apple'),
+    ).toBe(true);
+
+    expect(activityStore.getIngredientsForWeek(1).length).toBe(2);
+    expect(
+      activityStore.getIngredientsForWeek(1).some((ingredient) => ingredient.key === 'tomato'),
+    ).toBe(true);
+    expect(
+      activityStore.getIngredientsForWeek(1).some((ingredient) => ingredient.key === 'cucumber'),
+    ).toBe(true);
+
+    expect(activityStore.getIngredientsForWeek(2).length).toBe(0);
+  });
 });
