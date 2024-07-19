b265082ae0b4a8c39775688b0e425392a61b05b9
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-07-19 09:55:44 +0300

    Add test fixes

--- src/components/__tests__/LogView.spec.ts
+++ src/components/__tests__/LogView.spec.ts
@@ -1,10 +1,49 @@
-import {describe, it, expect} from 'vitest';
+import {describe, it, expect, beforeEach} from 'vitest';
 import {mount} from '@vue/test-utils';
+import {DateTime} from 'luxon';
 import LogView from '@/views/LogView.vue';
+import {useActivityStore} from '@/stores/activityStore';
 
 describe('LogView', () => {
+  let activityStore: ReturnType<typeof useActivityStore>;
+
+  beforeEach(() => {
+    activityStore = useActivityStore();
+  });
+
   it('renders', () => {
     const wrapper = mount(LogView);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('renders with data', () => {
+    const now = DateTime.now();
+    activityStore.settings.startDate = now.startOf('week').minus({weeks: 1});
+    activityStore.activities.push(
+      {
+        veggie: 'wheat',
+        date: now,
+      },
+      {
+        veggie: 'rye',
+        date: now.minus({weeks: 1}),
+      },
+      {
+        veggie: 'rice',
+        date: now.minus({weeks: 1}),
+      },
+      {
+        veggie: 'wheat',
+        date: now.minus({weeks: 1}),
+      },
+    );
+    const wrapper = mount(LogView, {
+      global: {
+        stubs: {
+          CategoryStatusChart: true,
+        },
+      },
+    });
     expect(wrapper.html()).toMatchSnapshot();
   });
 });
