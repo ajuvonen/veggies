d9b7dff44d5b30fdac55b7ab13c1cab949260c75
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-08-09 11:03:09 +0300

    Test appStateStore

---
+++ src/components/__tests__/appStateStore.spec.ts
@@ -0,0 +1,28 @@
+import {describe, it, expect, beforeEach} from 'vitest';
+import {useAppStateStore} from '@/stores/appStateStore';
+import {createPinia, setActivePinia} from 'pinia';
+
+describe('appStateStore', () => {
+  let appStateStore: ReturnType<typeof useAppStateStore>;
+
+  beforeEach(() => {
+    // creates a fresh pinia and makes it active
+    // so it's automatically picked up by any useStore() call
+    // without having to pass it to it: `useStore(pinia)`
+    setActivePinia(createPinia());
+    appStateStore = useAppStateStore();
+  });
+
+  it('adds messages', () => {
+    appStateStore.addToastMessage('hello');
+    expect(appStateStore.messages).toHaveLength(1);
+  });
+
+  it('removes messages', () => {
+    appStateStore.addToastMessage('hello');
+    appStateStore.addToastMessage('world');
+    appStateStore.removeToastMessage(appStateStore.messages[0].id);
+    expect(appStateStore.messages).toHaveLength(1);
+    expect(appStateStore.messages[0].text).toBe('world');
+  });
+});
