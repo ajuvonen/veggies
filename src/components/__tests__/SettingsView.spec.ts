ac75f1cf34033c3f25d8ee499b89231d1317ea61
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-08-10 09:15:48 +0300

    Warn about app reset and test its functionality

--- src/components/__tests__/SettingsView.spec.ts
+++ src/components/__tests__/SettingsView.spec.ts
@@ -1,10 +1,50 @@
-import {describe, it, expect} from 'vitest';
+import {describe, it, expect, beforeEach} from 'vitest';
 import {mount} from '@vue/test-utils';
+import {useAppStateStore} from '@/stores/appStateStore';
+import {useActivityStore} from '@/stores/activityStore';
 import SettingsView from '@/views/SettingsView.vue';
+import DialogStub from './DialogStub.vue';
+
+const mounter = () =>
+  mount(SettingsView, {
+    global: {
+      stubs: {
+        Dialog: DialogStub,
+        DialogPanel: {
+          template: '<div><slot /></div>',
+        },
+        DialogTitle: true,
+      },
+    },
+  });
 
 describe('SettingsView', () => {
+  let activityStore: ReturnType<typeof useActivityStore>;
+  let appStateStore: ReturnType<typeof useAppStateStore>;
+
+  beforeEach(() => {
+    activityStore = useActivityStore();
+    appStateStore = useAppStateStore();
+  });
+
   it('renders', () => {
     const wrapper = mount(SettingsView);
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('resets the app', async () => {
+    const wrapper = mounter();
+    await wrapper.findByTestId('reset-button').trigger('click');
+    await wrapper.findByTestId('confirm-button').trigger('click');
+    expect(appStateStore.$reset).toBeCalledTimes(1);
+    expect(activityStore.$reset).toBeCalledTimes(1);
+  });
+
+  it('cancels reset', async () => {
+    const wrapper = mounter();
+    await wrapper.findByTestId('reset-button').trigger('click');
+    await wrapper.findByTestId('cancel-button').trigger('click');
+    expect(appStateStore.$reset).toBeCalledTimes(0);
+    expect(activityStore.$reset).toBeCalledTimes(0);
   });
 });
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
+import {ref} from 'vue';
 import {useRouter} from 'vue-router';
 import {useActivityStore} from '@/stores/activityStore';
+import {useAppStateStore} from '@/stores/appStateStore';
 import LocaleChanger from '@/components/LocaleChanger.vue';
+import ModalDialog from '@/components/ModalDialog.vue';
 
 const router = useRouter();
 
-const {$reset} = useActivityStore();
+const {$reset: activityReset} = useActivityStore();
+const {$reset: appStateReset} = useAppStateStore();
+
+const resetDialogOpen = ref(false);
 
 const reset = () => {
-  $reset();
+  activityReset();
+  appStateReset();
   router.push({name: 'home'});
 };
 </script>
@@ -16,8 +23,33 @@ const reset = () => {
 <template>
   <LocaleChanger />
   <div class="flex-container justify-center">
-    <ButtonComponent variant="danger" icon="trashCan" @click="reset()">{{
-      $t('general.reset')
-    }}</ButtonComponent>
+    <ButtonComponent
+      variant="danger"
+      icon="trashCan"
+      data-test-id="reset-button"
+      @click="resetDialogOpen = true"
+      >{{ $t('general.reset') }}</ButtonComponent
+    >
   </div>
+  <ModalDialog
+    :open="resetDialogOpen"
+    :title="$t('settings.resetDialogTitle')"
+    @close="resetDialogOpen = false"
+  >
+    <template #content>
+      <p>{{ $t('settings.resetDialogText') }}</p>
+    </template>
+    <template #buttons>
+      <ButtonComponent data-test-id="cancel-button" @click="resetDialogOpen = false">{{
+        $t('general.cancel')
+      }}</ButtonComponent>
+      <ButtonComponent
+        data-test-id="confirm-button"
+        variant="danger"
+        icon="trashCan"
+        @click="reset()"
+        >{{ $t('general.reset') }}</ButtonComponent
+      >
+    </template>
+  </ModalDialog>
 </template>
