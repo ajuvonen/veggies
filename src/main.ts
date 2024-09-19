7599d14b5d1212b1b7dd4834b701daf25c7a2976
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-09-19 09:45:10 +0300

    Remove extra font variations

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import App from '@/App.vue';
 import router from '@/router';
 import i18n from '@/i18n';
 
-import '@fontsource/bungee-shade';
-import '@fontsource/nunito';
+import '@fontsource/bungee-shade/latin-400.css';
+import '@fontsource/nunito/latin-400.css';
 import '@/assets/main.scss';
 
 import IconComponent from '@/components/IconComponent.vue';
