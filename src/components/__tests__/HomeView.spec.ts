aca8e09913b67c8004ab1c80114ba03a94eccbb2
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-10-03 11:01:31 +0300

    Test home view dialog

--- src/components/__tests__/HomeView.spec.ts
+++ src/components/__tests__/HomeView.spec.ts
@@ -1,10 +1,28 @@
 import {describe, it, expect} from 'vitest';
 import {mount} from '@vue/test-utils';
 import HomeView from '@/views/HomeView.vue';
+import DialogStub from './DialogStub.vue';
 
 describe('HomeView', () => {
   it('renders', () => {
     const wrapper = mount(HomeView);
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('shows dialog', async () => {
+    const wrapper = mount(HomeView, {
+      global: {
+        stubs: {
+          Dialog: DialogStub,
+          DialogPanel: {
+            template: '<div><slot /></div>',
+          },
+          DialogTitle: true,
+        },
+      },
+    });
+    expect(wrapper.findByTestId('dialog').exists()).toBe(false);
+    await wrapper.findByTestId('home-info-button').trigger('click');
+    expect(wrapper.findByTestId('dialog').exists()).toBe(true);
   });
 });
