15c4dee5c916a8dda00bfe4fd35b38f7471f30b0
Anssi Juvonen <anssi.juvonen@gofore.com>
2024-10-10 15:21:54 +0300

    Update SASS

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
   plugins: [
     vue(),
     VitePWA({
@@ -53,7 +60,6 @@ export default defineConfig({
       },
     }),
   ],
-
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
