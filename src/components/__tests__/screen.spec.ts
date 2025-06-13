0f4685a83b2b70715fff4ec6def3aef149b5a121
Anssi Juvonen <anssi.juvonen@gofore.com>
2025-06-13 12:35:30 +0300

    Position toasts on top of onscreen keyboard

--- src/components/__tests__/screen.spec.ts
+++ src/components/__tests__/screen.spec.ts
@@ -34,4 +34,28 @@ describe('screen', () => {
     expect(wrapper.vm.maxHeightStyle).toBe('max-height: calc(768px - 100px - 1rem)');
     vi.restoreAllMocks();
   });
+
+  it('adds padding', async () => {
+    // Mock getBoundingClientRect
+    const mockGetBoundingClientRect = vi.spyOn(Element.prototype, 'getBoundingClientRect');
+    mockGetBoundingClientRect.mockReturnValue({
+      width: 300,
+      height: 500,
+      top: 100,
+      left: 50,
+      right: 350,
+      bottom: 600,
+      x: 50,
+      y: 100,
+      toJSON: () => ({}),
+    });
+
+    const wrapper = mount(ScreenTestComponent, {
+      props: {
+        padding: 0,
+      },
+    });
+    expect(wrapper.vm.maxHeightStyle).toBe('max-height: calc(768px - 100px - 0rem)');
+    vi.restoreAllMocks();
+  });
 });
--- src/hooks/screen.ts
+++ src/hooks/screen.ts
@@ -1,13 +1,16 @@
 import {computed, type ComponentPublicInstance, type Ref} from 'vue';
 import {useElementBounding, useWindowSize} from '@vueuse/core';
 
-export function useScreen(element: Ref<HTMLElement | ComponentPublicInstance | null>) {
+export function useScreen(
+  element: Ref<HTMLElement | ComponentPublicInstance | null>,
+  bottomPadding: number = 1,
+) {
   const {height} = useWindowSize({type: 'visual'});
 
   const {top} = useElementBounding(element);
 
   const maxHeightStyle = computed(
-    () => `max-height: calc(${height.value}px - ${top.value}px - 1rem)`,
+    () => `max-height: calc(${height.value}px - ${top.value}px - ${bottomPadding}rem)`,
   );
 
   return {
