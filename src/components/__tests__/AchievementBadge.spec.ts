575b7593bb3d20248e08b3a1a970b8cb94f2a2d6
Anssi Juvonen <anssi.juvonen@gofore.com>
2025-06-13 09:22:54 +0300

    Show weekly achievement progress in the editor

--- src/components/__tests__/AchievementBadge.spec.ts
+++ src/components/__tests__/AchievementBadge.spec.ts
@@ -27,4 +27,35 @@ describe('AchievementBadge', () => {
 
     expect(wrapper.html()).toMatchSnapshot();
   });
+
+  it('does not render gradient when achievement is active', () => {
+    const wrapper = mount(AchievementBadge, {
+      props: {
+        achievement: 'goNuts',
+        level: AchievementLevel.Gold,
+        active: true,
+        degree: 360,
+      },
+    });
+    expect(wrapper.find('.badge__overlay').exists()).toBe(false);
+  });
+
+  it('renders gradient', async () => {
+    const wrapper = mount(AchievementBadge, {
+      props: {
+        achievement: 'thirtyVeggies',
+        level: AchievementLevel.Gold,
+        active: false,
+        degree: 180,
+      },
+    });
+
+    expect(wrapper.find('.badge__overlay').attributes('style')).toBe(
+      'mask-image: conic-gradient(transparent 0deg 180deg, black 180deg 360deg);',
+    );
+    await wrapper.setProps({degree: 120});
+    expect(wrapper.find('.badge__overlay').attributes('style')).toBe(
+      'mask-image: conic-gradient(transparent 0deg 120deg, black 120deg 360deg);',
+    );
+  });
 });
