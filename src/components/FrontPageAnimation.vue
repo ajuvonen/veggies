6d597fc59963d95d4ce5c64b7bfe9487eb0d4890
Anssi Juvonen <anssi.juvonen@gofore.com>
2025-08-19 13:28:39 +0300

    Add an E2E test for week summary dialog

--- src/components/FrontPageAnimation.vue
+++ src/components/FrontPageAnimation.vue
@@ -13,7 +13,11 @@ let emojis = getRandomEmojis(4);
 </script>
 <template>
   <div class="front-page-animation__container" aria-hidden="true">
-    <div v-if="reducedMotion !== 'reduce'" class="front-page-animation">
+    <div
+      v-if="reducedMotion !== 'reduce'"
+      class="front-page-animation"
+      data-test-id="front-page-animation"
+    >
       <template v-for="index in 2" :key="index">
         <div v-for="emoji in emojis" :key="emoji" class="front-page-animation__icon-container">
           <span class="front-page-animation__icon">{{ emoji }}</span>
