189e149b45db91ce08d1c44c4ebe1d6725f88a31
Anssi Juvonen <anssi.juvonen@gofore.com>
2025-08-24 16:32:55 +0300

    Calculate rainbow achievement progress

--- src/hooks/achievementCompletion.ts
+++ src/hooks/achievementCompletion.ts
@@ -1,16 +1,28 @@
-import {BOTANICAL_BERRIES, CITRUSES, NUTS} from '@/utils/constants';
-import type {Achievements} from '@/utils/types';
-import {intersection} from 'remeda';
 import {computed, type Ref} from 'vue';
+import {countBy, intersection} from 'remeda';
+import {BOTANICAL_BERRIES, CITRUSES, NUTS} from '@/utils/constants';
+import {getCategoryForVeggie} from '@/utils/helpers';
+import {Category, type Achievements} from '@/utils/types';
 
 export function useAchievementCompletion(veggies: Ref<string[]>) {
   const degreeFormatter = (multiplier: number) => Math.floor(Math.min(multiplier * 360, 360));
 
-  const weeklyCompletion = computed<Partial<Record<keyof Achievements, number>>>(() => ({
-    botanicalBerries: degreeFormatter(intersection(BOTANICAL_BERRIES, veggies.value).length / 15),
-    lemons: degreeFormatter(intersection(CITRUSES, veggies.value).length / 5),
-    goNuts: degreeFormatter(intersection(NUTS, veggies.value).length / 5),
-    thirtyVeggies: degreeFormatter(veggies.value.length / 30),
-  }));
+  const weeklyCompletion = computed<Partial<Record<keyof Achievements, number>>>(() => {
+    const groupedVeggies = countBy(veggies.value, getCategoryForVeggie);
+    const categories = Object.values(Category);
+    return {
+      botanicalBerries: degreeFormatter(intersection(BOTANICAL_BERRIES, veggies.value).length / 15),
+      lemons: degreeFormatter(intersection(CITRUSES, veggies.value).length / 5),
+      goNuts: degreeFormatter(intersection(NUTS, veggies.value).length / 5),
+      rainbow: degreeFormatter(
+        categories.reduce(
+          (count, current) =>
+            groupedVeggies[current] && groupedVeggies[current] >= 3 ? count + 1 : count,
+          0,
+        ) / categories.length,
+      ),
+      thirtyVeggies: degreeFormatter(veggies.value.length / 30),
+    };
+  });
   return {weeklyCompletion};
 }
