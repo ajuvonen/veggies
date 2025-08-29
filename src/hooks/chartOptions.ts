c824a00f9c3617457f19981131a0bf13db8148c7
Anssi Juvonen <anssi.juvonen@gofore.com>
2025-08-29 08:42:41 +0300

    Reduce computed style calls

--- src/hooks/chartOptions.ts
+++ src/hooks/chartOptions.ts
@@ -13,112 +13,102 @@ export function useChartOptions<T extends ChartType>(
   overrides: MaybeRefOrGetter<Partial<ChartOptions<T>>>,
 ) {
   const {showChartAnimations} = useChartAnimations();
-  const chartOptions = computed(
-    () =>
-      mergeDeep(
-        {
-          animation: !showChartAnimations.value ? false : undefined,
-          responsive: true,
-          maintainAspectRatio: !showGrid,
-          normalized: true,
-          layout: {
-            padding: 0,
+  const chartOptions = computed(() => {
+    const textColor = getComputedStyle(document.documentElement).getPropertyValue('--color-text');
+    return mergeDeep(
+      {
+        animation: !showChartAnimations.value ? false : undefined,
+        responsive: true,
+        maintainAspectRatio: !showGrid,
+        normalized: true,
+        layout: {
+          padding: 0,
+        },
+        scales: showGrid
+          ? {
+              y: {
+                beginAtZero: true,
+                ticks: {
+                  precision: 0,
+                  color: textColor,
+                },
+                stacked,
+              },
+              y1: {
+                position: 'right',
+                beginAtZero: true,
+                ticks: {
+                  precision: 0,
+                  color: textColor,
+                },
+                stacked,
+                afterBuildTicks: (axis: Scale) => {
+                  axis.ticks = [...axis.chart.scales.y.ticks];
+                  axis.min = axis.chart.scales.y.min;
+                  axis.max = axis.chart.scales.y.max;
+                },
+              },
+              x: {
+                beginAtZero: true,
+                ticks: {
+                  precision: 0,
+                  color: textColor,
+                },
+                stacked,
+              },
+            }
+          : undefined,
+        plugins: {
+          title: {
+            display: false,
           },
-          scales: showGrid
-            ? {
-                y: {
-                  beginAtZero: true,
-                  ticks: {
-                    precision: 0,
-                    color: getComputedStyle(document.documentElement).getPropertyValue(
-                      '--color-text',
-                    ),
+          legend: {
+            display: false,
+          },
+          datalabels: {
+            ...(showCategoryEmoji
+              ? {
+                  anchor: 'center',
+                  align: 'center',
+                  font: {
+                    size: 25,
                   },
-                  stacked,
-                },
-                y1: {
-                  position: 'right',
-                  beginAtZero: true,
-                  ticks: {
-                    precision: 0,
-                    color: getComputedStyle(document.documentElement).getPropertyValue(
-                      '--color-text',
-                    ),
-                  },
-                  stacked,
-                  afterBuildTicks: (axis: Scale) => {
-                    axis.ticks = [...axis.chart.scales.y.ticks];
-                    axis.min = axis.chart.scales.y.min;
-                    axis.max = axis.chart.scales.y.max;
-                  },
-                },
-                x: {
-                  beginAtZero: true,
-                  ticks: {
-                    precision: 0,
-                    color: getComputedStyle(document.documentElement).getPropertyValue(
-                      '--color-text',
-                    ),
-                  },
-                  stacked,
-                },
-              }
-            : undefined,
-          plugins: {
-            title: {
-              display: false,
+                  textShadowColor: '#fff',
+                  textShadowBlur: 5,
+                  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                  formatter: (value: any, {dataset: {label}}: Context) =>
+                    value ? CATEGORY_EMOJI[label as Category] : '',
+                }
+              : {
+                  display: false,
+                }),
+          },
+          tooltip: {
+            animation: showChartAnimations.value,
+            padding: 8,
+            titleFont: {
+              size: 14,
+              weight: 'normal',
             },
-            legend: {
-              display: false,
+            bodyFont: {
+              size: 14,
             },
-            datalabels: {
-              ...(showCategoryEmoji
-                ? {
-                    anchor: 'center',
-                    align: 'center',
-                    font: {
-                      size: 25,
-                    },
-                    textShadowColor: '#fff',
-                    textShadowBlur: 5,
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    formatter: (value: any, {dataset: {label}}: Context) =>
-                      value ? CATEGORY_EMOJI[label as Category] : '',
-                  }
-                : {
-                    display: false,
-                  }),
+            footerFont: {
+              size: 14,
+              weight: 'normal',
             },
-            tooltip: {
-              animation: showChartAnimations.value,
-              padding: 8,
-              titleFont: {
-                size: 14,
-                weight: 'normal',
-              },
-              bodyFont: {
-                size: 14,
-              },
-              footerFont: {
-                size: 14,
-                weight: 'normal',
-              },
-              displayColors: false,
-              backgroundColor: getComputedStyle(document.documentElement).getPropertyValue(
-                '--color-ui-dark',
-              ),
-              bodyColor: getComputedStyle(document.documentElement).getPropertyValue(
-                '--color-text',
-              ),
-              titleColor: getComputedStyle(document.documentElement).getPropertyValue(
-                '--color-text',
-              ),
-            },
+            displayColors: false,
+            backgroundColor: getComputedStyle(document.documentElement).getPropertyValue(
+              '--color-ui-dark',
+            ),
+            bodyColor: textColor,
+            titleColor: textColor,
           },
         },
-        toValue(overrides),
-      ) as ChartOptions<T>,
-  );
+      },
+      toValue(overrides),
+    ) as ChartOptions<T>;
+  });
 
   return {
     chartOptions,
