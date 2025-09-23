ba2bdcf6a5ce1c51d311af0dbe3a1e3c179498ff
Anssi Juvonen <anssi.juvonen@gofore.com>
2025-09-23 15:39:29 +0300

    Extract toggle to its own component

---
+++ src/components/ToggleComponent.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {Switch} from '@headlessui/vue';
+const model = defineModel<boolean>({required: true});
+withDefaults(
+  defineProps<{
+    disabled?: boolean;
+  }>(),
+  {
+    disabled: false,
+  },
+);
+</script>
+<template>
+  <Switch v-model="model" :disabled="disabled" class="toggle-button">
+    <div>
+      <IconComponent :icon="model ? 'check' : 'close'" />
+    </div>
+  </Switch>
+</template>
+<style>
+.toggle-button {
+  @apply relative inline-flex h-4 w-12 items-center rounded-md outline-offset-4;
+  @apply bg-[--color-ui-dark];
+
+  &[disabled] {
+    @apply opacity-50 cursor-not-allowed pointer-events-none;
+  }
+
+  > div {
+    @apply inline-block w-6 h-6 transform rounded-md transition-transform;
+    @apply flex items-center justify-center;
+    @apply bg-[--color-bg-alternative] fill-[--color-text-alternative] active:bg-[--color-primary-active] hover:bg-[--color-primary-hover] hover:fill-[--color-text];
+  }
+
+  &[data-headlessui-state='checked'] > div {
+    @apply translate-x-6;
+    @apply bg-[--color-primary] fill-[--color-text] active:bg-[--color-primary-active] hover:bg-[--color-primary-hover];
+  }
+}
+</style>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -2,7 +2,6 @@
 import {ref} from 'vue';
 import {useRouter} from 'vue-router';
 import {storeToRefs} from 'pinia';
-import {Switch} from '@headlessui/vue';
 import {useChartAnimations} from '@/hooks/chartAnimations';
 import {useActivityStore} from '@/stores/activityStore';
 import {useAppStateStore} from '@/stores/appStateStore';
@@ -13,6 +12,7 @@ import ExportImport from '@/components/ExportImport.vue';
 import BuildTime from '@/components/BuildTime.vue';
 import VeggieSearch from '@/components/VeggieSearch.vue';
 import TagsComponent from '@/components/TagsComponent.vue';
+import ToggleComponent from '@/components/ToggleComponent.vue';
 
 const router = useRouter();
 
@@ -60,16 +60,13 @@ const removeAllergen = (veggie: string) => {
       :labelAttrs="{for: 'show-animations-button'}"
       labelTag="label"
     >
-      <Switch
+      <ToggleComponent
         id="show-animations-button"
         v-model="showChartAnimations"
         :disabled="reduceMotion"
         data-test-id="show-animations-button"
       >
-        <div class="show-animations__toggler">
-          <IconComponent :icon="showChartAnimations ? 'check' : 'close'" />
-        </div>
-      </Switch>
+      </ToggleComponent>
     </ContentElement>
     <ContentElement
       :title="$t('settings.allergens')"
@@ -140,26 +137,6 @@ const removeAllergen = (veggie: string) => {
   &::-moz-range-thumb {
     @apply appearance-none rounded-md border-none w-6 h-6 cursor-pointer;
     @apply bg-[--color-primary] hover:bg-[--color-primary-hover] active:bg-[--color-primary-active];
-  }
-}
-
-:deep(#show-animations-button) {
-  @apply relative inline-flex h-4 w-12 items-center rounded-md outline-offset-4;
-  @apply bg-[--color-ui-dark];
-
-  &[disabled] {
-    @apply opacity-50 cursor-not-allowed pointer-events-none;
-  }
-
-  > div {
-    @apply inline-block w-6 h-6 transform rounded-md transition-transform;
-    @apply flex items-center justify-center;
-    @apply bg-[--color-bg-alternative] fill-[--color-text-alternative] active:bg-[--color-primary-active] hover:bg-[--color-primary-hover] hover:fill-[--color-text];
-  }
-
-  &[data-headlessui-state='checked'] > div {
-    @apply translate-x-6;
-    @apply bg-[--color-primary] fill-[--color-text] active:bg-[--color-primary-active] hover:bg-[--color-primary-hover];
   }
 }
 </style>
