cc8d17c35b301636486c89a82f1ee28ae1445a06
Anssi Juvonen <anssi.juvonen@gofore.com>
2025-10-23 13:50:26 +0300

    Remove relative import

--- src/components/component.d.ts
+++ src/components/component.d.ts
@@ -1,6 +1,6 @@
 import type ButtonComponent from '@/components/ButtonComponent.vue';
 import type IconComponent from '@/components/IconComponent.vue';
-import type ContentElement from './ContentElement.vue';
+import type ContentElement from '@/components/ContentElement.vue';
 
 declare module 'vue' {
   export interface GlobalComponents {
