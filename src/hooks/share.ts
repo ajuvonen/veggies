208eb0a7b372a5a76e92b80c81f103cadcdc50a3
Anssi Juvonen <anssi.juvonen@gofore.com>
2026-01-03 13:38:57 +0200

    Add composable sharing hook

---
+++ src/hooks/share.ts
@@ -0,0 +1,21 @@
+import {useI18n} from 'vue-i18n';
+import {APP_URL} from '@/utils/constants';
+
+export function useShare() {
+  const {t} = useI18n();
+
+  const shareSupported = !!navigator.share;
+
+  const shareOrCopy = async (shareKey: string, shareProps: (string | number)[]) =>
+    shareSupported
+      ? await navigator.share({
+          text: t(shareKey, shareProps).trim(),
+          url: APP_URL,
+        })
+      : navigator.clipboard.writeText(t(shareKey, [...shareProps, APP_URL]));
+
+  return {
+    shareOrCopy,
+    shareSupported,
+  };
+}
