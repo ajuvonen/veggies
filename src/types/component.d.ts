d4d2e39a4153118c23eb2fd3f0250da479c8ddc0
Anssi Juvonen <anssi.juvonen@gofore.com>
2026-01-13 09:28:46 +0200

    Lazy-load global components

--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,11 +1,11 @@
 import type ButtonComponent from '@/components/ButtonComponent.vue';
-import type IconComponent from '@/components/IconComponent.vue';
 import type ContentElement from '@/components/ContentElement.vue';
+import type IconComponent from '@/components/IconComponent.vue';
 
 declare module 'vue' {
   export interface GlobalComponents {
     ButtonComponent: ButtonComponent;
-    IconComponent: IconComponent;
     ContentElement: ContentElement;
+    IconComponent: IconComponent;
   }
 }
