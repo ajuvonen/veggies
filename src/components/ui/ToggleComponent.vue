d46e5122b22d832913331bd476fe126246d80010
Anssi Juvonen <anssi.juvonen@gofore.com>
2026-06-05 15:42:29 +0300

    Describe ToggleComponent with tailwind-variants

--- src/components/ui/ToggleComponent.vue
+++ src/components/ui/ToggleComponent.vue
@@ -1,42 +1,45 @@
 <script setup lang="ts">
+import {tv} from 'tailwind-variants/lite';
+
 const model = defineModel<boolean>({required: true});
-withDefaults(
-  defineProps<{
-    disabled?: boolean;
-  }>(),
-  {
-    disabled: false,
+const props = defineProps<{disabled?: boolean}>();
+
+const toggle = tv({
+  slots: {
+    root: 'relative inline-flex h-4 w-12 items-center rounded-md outline-offset-4 bg-[--color-ui-dark]',
+    thumb: [
+      'inline-flex w-6 h-6 items-center justify-center transform rounded-md shadow-lg transition-transform duration-200',
+      'bg-[--color-bg-alternative] fill-[--color-text-alternative]',
+    ],
   },
-);
+  variants: {
+    disabled: {
+      true: {
+        root: 'opacity-50 cursor-not-allowed',
+      },
+      false: {
+        thumb:
+          'hover:bg-[--color-primary-hover] hover:fill-[--color-text] active:bg-[--color-primary-active] active:fill-[--color-text]',
+      },
+    },
+    checked: {
+      true: {
+        thumb: 'translate-x-6 bg-[--color-primary] fill-[--color-text]',
+      },
+    },
+  },
+  defaultVariants: {
+    disabled: false,
+    checked: false,
+  },
+});
+
+const {root, thumb} = toggle();
 </script>
 <template>
-  <SwitchRoot v-model="model" :disabled="disabled" class="toggle-button">
-    <SwitchThumb class="toggle-button__thumb">
+  <SwitchRoot v-model="model" :disabled="disabled" :class="root({disabled, checked: model})">
+    <SwitchThumb :class="thumb({disabled, checked: model})">
       <IconComponent :icon="model ? 'check' : 'close'" />
     </SwitchThumb>
   </SwitchRoot>
 </template>
-<style>
-.toggle-button {
-  @apply relative inline-flex h-4 w-12 items-center rounded-md outline-offset-4;
-  @apply bg-[--color-ui-dark];
-
-  &[disabled] {
-    @apply opacity-50 cursor-not-allowed;
-  }
-
-  > .toggle-button__thumb {
-    @apply inline-block w-6 h-6 transform rounded-md shadow-lg transition-transform duration-200;
-    @apply flex items-center justify-center;
-    @apply bg-[--color-bg-alternative] fill-[--color-text-alternative];
-
-    [data-state='checked']& {
-      @apply translate-x-6;
-      @apply bg-[--color-primary] fill-[--color-text];
-    }
-    :not([disabled])& {
-      @apply hover:bg-[--color-primary-hover] hover:fill-[--color-text] active:bg-[--color-primary-active] active:fill-[--color-text];
-    }
-  }
-}
-</style>
