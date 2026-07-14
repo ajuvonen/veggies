91e4cb8c29a4dbc550bac1deb37f9cc8743cd0eb
Anssi Juvonen <anssi.juvonen@gofore.com>
2026-07-14 08:22:34 +0300

    Return default font

--- src/components/charts/HeatmapChart.vue
+++ src/components/charts/HeatmapChart.vue
@@ -3,6 +3,7 @@ import {Chart as ChartJS, CategoryScale, Tooltip} from 'chart.js';
 import {MatrixController, MatrixElement} from 'chartjs-chart-matrix';
 import {createTypedChart} from 'vue-chartjs';
 
+ChartJS.defaults.font.family = 'Nunito';
 ChartJS.register(CategoryScale, MatrixController, MatrixElement, Tooltip);
 
 export default createTypedChart('matrix', MatrixController);
